1357eaeee4141c3e6e0994d0e44d17932be353f2
krodev <kfd.dev@gmail.com>
2017-06-11 03:35:28 -0400

    Mejora en admin.py orden de codigo

--- src/apps/boletin/admin.py
+++ src/apps/boletin/admin.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from .models import Registrado
 from .forms import RegModelForm
 # Register your models here.
-
 class AdminRegistrado(admin.ModelAdmin):
     list_display = ["email", "nombre","timestamp"] #que campos mostrar en el formulario
     form = RegModelForm #atender aca.. de donde estira
@@ -10,8 +9,6 @@ class AdminRegistrado(admin.ModelAdmin):
     list_filter = ["timestamp"]
     #list_editable =["nombre"] #no se puede cambiar el email si es que es el link tambien
     search_fields = ["email","nombre"]
-
     # class Meta:
     #     model = Registrado
-
 admin.site.register(Registrado, AdminRegistrado)
