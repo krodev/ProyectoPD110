04ebd87bb9eb560d80da39557fc7f6474eac9762
krodev <kfd.dev@gmail.com>
2017-06-11 13:58:39 -0400

    Create settings.py

--- src/pd110/settings.py
+++ src/pd110/settings.py
@@ -29,7 +29,7 @@ ALLOWED_HOSTS = ['*']
 
 #para enviar mensaje o email
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'kfd.dev@gmail.com'
+EMAIL_HOST_USER = '****@gmail.com'
 EMAIL_HOST_PASSWORD = 'vfjbizxckxrqrqgx'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
